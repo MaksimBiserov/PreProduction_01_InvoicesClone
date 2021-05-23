0caac32054ae7664e0db2ab630872feb2839469a
MaksimBiserov <maxtyge@mail.ru>
2021-05-23 15:37:55 +0400

    Added full logic

--- force-app/main/default/aura/cloneWithDetailsAction/cloneWithDetailsActionHelper.js
+++ force-app/main/default/aura/cloneWithDetailsAction/cloneWithDetailsActionHelper.js
@@ -1,5 +1,19 @@
 ({
-    helperMethod : function() {
+	createRecord: function(component, event) {
+		let createRecordEvent = $A.get("e.force:createRecord");
+		let dfv = '{' + component.get("v.defaultFieldsValues").toString() +
+				  ', "IsDeepClone__c" : true}';
+		dfv = dfv.replaceAll("'", '"');
 
-    }
+		console.log('dfv: ' + dfv);
+		console.log('dfv value: ' + valueOf(dfv));
+
+		createRecordEvent.setParams({
+		    "entityApiName": "Invoice__c",
+			"defaultFieldValues": JSON.parse(dfv)
+		});
+
+		createRecordEvent.fire();
+	    $A.get("e.force:closeQuickAction").fire();
+   }
 })
